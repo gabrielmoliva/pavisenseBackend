75a5a9ed77293c9aab144271201c9cfe8a1742b1
gabrielmoliva <moliva.gabriel@gmail.com>
2025-07-29 21:24:20 -0300

    Mudando pra o SQLModel

--- app/database.py
+++ app/database.py
@@ -1,8 +1,13 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+# Arquivo de conexão ao banco de dados
+
+from sqlmodel import SQLModel, create_engine, Session
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./pavisense.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
