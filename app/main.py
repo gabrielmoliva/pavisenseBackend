a9b4bb6860ba2fa2cd1206e271166054ab46445a
gabrielmoliva <moliva.gabriel@gmail.com>
2025-08-22 17:26:45 -0300

    print dados

--- app/main.py
+++ app/main.py
@@ -46,7 +46,7 @@ async def websocket_endpoint(ws: WebSocket, db: db_dependency, user_id: int, mod
         while True:
             data = await ws.receive_json()
             dados = Dados(**data)
-            # print(dados)
+            print(dados)
             if dados.speed < MIN_SPEED_VALUE:
                 continue
 
